e37ca93c34886ad1843ca8d6ef033a69a6f27746
washlopes <washlopes@gmail.com>
2021-04-01 18:04:38 -0300

    Criado TesteComponent e movido para components

---
+++ src/components/TesteComponents/TesteComponent.tsx
@@ -0,0 +1,9 @@
+import React from   'react'
+
+import './TesteComponent.css'
+
+const TesteComponent = () => (
+
+  <div className="TesteComponent">Teste Component!!!!</div>
+)
+export default TesteComponent
