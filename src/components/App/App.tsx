494715b58991fd1f3930a9d32a58867816dec74d
washlopes <washlopes@gmail.com>
2021-04-04 09:29:02 -0300

    ciclos de vida

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,10 +9,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        
           <TesteComponent />
           <ClassComponent name="Alexandra"/>
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
