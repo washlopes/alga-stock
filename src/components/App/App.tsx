d4a215f7806ebf162c387fb247aa235f2a1d4b2d
washlopes <washlopes@gmail.com>
2021-04-05 14:17:45 -0300

    Inclusão de estilos css na aplicação

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../Header';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       App
     </div>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return <header className="AppHeader">
+    <h1>AlgaStock</h1>
+  </header>
+}
+
+export default Header
