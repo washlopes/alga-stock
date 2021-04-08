b1c2cc956b8c4b8995c8ffe36b4cd03e6aeb418f
washlopes <washlopes@gmail.com>
2021-04-08 09:26:43 -0300

    Criação da Table Estilizada e criação de SASS

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -4,25 +4,24 @@ import './Table.scss'
 // import { Container } from './styles';
 
 const Table: React.FC = () => {
-  return <table>
+  return <table className="AppTable">
     <thead>
       <tr>
         <th>Product</th>
         <th>Price</th>
-        <th>Stock</th>
-        <th></th>
+        <th className="right">Stock</th>        
       </tr>
     </thead>
     <tbody>
       <tr>
         <td>Cookie</td>
         <td>$1.25</td>
-        <td>23</td>
+        <td className="right">23</td>
       </tr>
       <tr>
         <td>Milk</td>
         <td>$.99</td>
-        <td>10</td>
+        <td className="right">10</td>
       </tr>
     </tbody>
     </table>  
