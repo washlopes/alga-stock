24cc2d1b6034a1c603ff0440769c3cd93746994e
washlopes <washlopes@gmail.com>
2021-04-08 07:25:05 -0300

    Limpeza App base, criação Component Table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input';
 import Header from '../Header';
 import './App.css';
 
-function TestComponent()  {
-  return <img width="16" src="https://a-static.mlcdn.com.br/1500x1500/lupa-de-mao-100mm-aumento-para-leitura-observacao-bancada-mbtech/badmintonstore/c9d874dac4c611eaa8de4201ac18501e/66485b56c7874c4d625cd0de188b45dd.jpg" alt="Ícone Pesquisar" />
-}
-
-function App() {
-
-  const [nome, setNome] = useState('')
+function App() { 
 
   return (
     <div className="App">
       <Header title="AlgaStock"/>
       <Container >
-        <Input 
-          label="Nome"
-          placeholder="Nome"
-          value={nome}
-          onChange = { e => setNome(e.target.value)} />
-        <Button  onClick= { () => window.alert('Uhuuuuuu....') }
-          appendIcon={<TestComponent />}
-        >
-          Submit
-        </Button>
+        
       </Container>            
     </div>
     
---
+++ src/shared/Table/Table.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Table.scss'
+// import { Container } from './styles';
+
+const Table: React.FC = () => {
+  return <div />
+  
+}
+
+export default Table;
