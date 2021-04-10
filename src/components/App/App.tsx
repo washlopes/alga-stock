2aca817171ccbef8c67c2e933e7c9fe66172f869
washlopes <washlopes@gmail.com>
2021-04-10 07:58:18 -0300

    Criando Props da Tabela

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 import Container from '../../shared/Container';
-import Table from '../../shared/Table/Table';
+import Table, { TableHeader } from '../../shared/Table';
+import Products from '../../shared/Table/Table.mockdata';
+
 import Header from '../Header';
 import './App.css';
+
+const headers: TableHeader[] = [
+  { key: 'id', value: '#' },
+  { key: 'name', value: 'Product' },
+  { key: 'price', value: 'Price', right: true },
+  { key: 'stock', value: 'Available Stock', right: true },
+  //{ key: 'action', value: "Action"}
+]
 
 function App() { 
 
@@ -11,7 +21,12 @@ function App() {
     <div className="App">
       <Header title="AlgaStock"/>
       <Container >
-        <Table />
+        <Table 
+          headers={headers}
+          data={Products}
+        >
+
+        </Table>
       </Container>            
     </div>
     
