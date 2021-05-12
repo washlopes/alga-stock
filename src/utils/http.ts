bbdb782cc13bde66c36cff216b8e7a36637ea38d
washlopes <washlopes@gmail.com>
2021-05-12 11:16:42 -0300

    ajustes

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,11 +1,17 @@
 import axios from 'axios'
+import { store } from '../redux'
 
 const http = axios.create({
     //baseURL: 'https://jsonplaceholder.typicode.com'
-    baseURL: 'http://localhost:3024',
-    headers: {
-      authorization: 'Bearer 123'
-    }
+    baseURL: 'http://localhost:3024'    
   })
 
+http.interceptors.request.use((config) => {
+  const token = store.getState().authentication.profile?.token
+
+  if (token)
+    config.headers.authorized = `Bearer ${token}`
+  
+    return config
+})
 export default http
